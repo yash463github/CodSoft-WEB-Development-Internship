3d86742940b72620bd1bb06775de98131265547b
Yash Dhumal <132649136+yash463github@users.noreply.github.com>
2024-01-02 00:42:07 +0530

    Add files via upload

---
+++ Personal PortFolio/pf.js
@@ -0,0 +1,7 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+<script>
+    var typed = new Typed('#element', {
+        strings: ['Web Developer', 'Graphic Designer', 'Game Designer'],
+        typeSpeed: 50,
+    });
+</script>
